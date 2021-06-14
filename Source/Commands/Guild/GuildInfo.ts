02a685cc6d3b618711520f8383ff3b1267076bc7
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-14 23:23:08 +0530

    Mentioned Owner & removed in :- in Guild Info command

--- Source/Commands/Guild/GuildInfo.ts
+++ Source/Commands/Guild/GuildInfo.ts
@@ -17,21 +17,21 @@ export default class ServerInfo extends BaseCommand {
 			// @ts-ignore
 			.setTitle(message.guild.name)
 			// @ts-ignore
-			.addField("Owner:- ", `${message.guild?.ownerID}`, true)
-			.addField("ID:- ", `${message.guild?.id}`, true)
-			.addField("Created At:- ", `${message.guild?.createdAt.toLocaleDateString("en-us")} ${message.guild?.createdAt.toLocaleTimeString("en-us")}`)
-			.addField("Region:- ", "India", true)
-			.addField("Verification Level:- ", `${message.guild?.verificationLevel}`, true)
-			.addField("Total Members:- ", `${message.guild?.memberCount}`, true)
-			.addField("Online Members:- ", `${message.guild?.members.cache.filter(m => m.user.presence.status == "online").size}`, true)
-			.addField("Bots:- ", `${message.guild?.members.cache.filter(m => m.user.bot).size}`, true)
+			.addField("Owner ", `<@!${message.guild?.ownerID}>`, true)
+			.addField("ID ", `${message.guild?.id}`, true)
+			.addField("Created At ", `${message.guild?.createdAt.toLocaleDateString("en-us")} ${message.guild?.createdAt.toLocaleTimeString("en-us")}`)
+			.addField("Region ", "India", true)
+			.addField("Verification Level ", `${message.guild?.verificationLevel}`, true)
+			.addField("Total Members ", `${message.guild?.memberCount}`, true)
+			.addField("Online Members ", `${message.guild?.members.cache.filter(m => m.user.presence.status == "online").size}`, true)
+			.addField("Bots ", `${message.guild?.members.cache.filter(m => m.user.bot).size}`, true)
 			// @ts-ignore
-			.addField("Boosters:- ", `${message.guild?.premiumSubscriptionCount}`, true)
-			.addField("Roles:- ", `${message.guild?.roles.cache.size}`, true)
-			.addField("Channels:- ", `${message.guild?.channels.cache.size}`, true)
-			.addField("Text Channels:- ", `${message.guild?.channels.cache.filter(r => r.type === "text").size}`, true)
-			.addField("Voice Channels:- ", `${message.guild?.channels.cache.filter(c => c.type === "voice").size}`)
-			.addField("Emojis:- ", `${message.guild?.emojis.cache.size}`, true);
+			.addField("Boosters ", `${message.guild?.premiumSubscriptionCount}`, true)
+			.addField("Roles ", `${message.guild?.roles.cache.size}`, true)
+			.addField("Channels ", `${message.guild?.channels.cache.size}`, true)
+			.addField("Text Channels ", `${message.guild?.channels.cache.filter(r => r.type === "text").size}`, true)
+			.addField("Voice Channels ", `${message.guild?.channels.cache.filter(c => c.type === "voice").size}`, true)
+			.addField("Emojis ", `${message.guild?.emojis.cache.size}`, true);
 
 		this.client.sendEmbed(message, serverInfoEmbed);
 	}
