1e289b69f90db1274e6b34de904edf60ff123687
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-14 23:20:20 +0530

    Fixed up Guild Info command, need to make more changes

--- Source/Commands/Guild/GuildInfo.ts
+++ Source/Commands/Guild/GuildInfo.ts
@@ -1,4 +1,4 @@
-import { Message, MessageEmbed } from "discord.js";
+import { Message } from "discord.js";
 import BaseCommand from "../../Base/BaseCommand";
 import CodeFictionistClient from "../../Base/Client";
 
@@ -11,15 +11,12 @@ export default class ServerInfo extends BaseCommand {
 		});
 	}
 	async run(message: Message) {
-
-		const embed = new MessageEmbed()
-			.setTitle("Server Stats")
-		// @ts-ignore
-			.setAuthor("CodeFiction", message.guild?.iconURL())
-		// @ts-ignore
+		const serverInfoEmbed = this.client.embed(message.author)
+			// @ts-ignore
 			.setThumbnail(message.guild?.iconURL())
-			.setColor("RANDOM")
-		// @ts-ignore
+			// @ts-ignore
+			.setTitle(message.guild.name)
+			// @ts-ignore
 			.addField("Owner:- ", `${message.guild?.ownerID}`, true)
 			.addField("ID:- ", `${message.guild?.id}`, true)
 			.addField("Created At:- ", `${message.guild?.createdAt.toLocaleDateString("en-us")} ${message.guild?.createdAt.toLocaleTimeString("en-us")}`)
@@ -28,16 +25,14 @@ export default class ServerInfo extends BaseCommand {
 			.addField("Total Members:- ", `${message.guild?.memberCount}`, true)
 			.addField("Online Members:- ", `${message.guild?.members.cache.filter(m => m.user.presence.status == "online").size}`, true)
 			.addField("Bots:- ", `${message.guild?.members.cache.filter(m => m.user.bot).size}`, true)
-		// @ts-ignore
+			// @ts-ignore
 			.addField("Boosters:- ", `${message.guild?.premiumSubscriptionCount}`, true)
 			.addField("Roles:- ", `${message.guild?.roles.cache.size}`, true)
 			.addField("Channels:- ", `${message.guild?.channels.cache.size}`, true)
 			.addField("Text Channels:- ", `${message.guild?.channels.cache.filter(r => r.type === "text").size}`, true)
 			.addField("Voice Channels:- ", `${message.guild?.channels.cache.filter(c => c.type === "voice").size}`)
-			.addField("Emojis:- ", `${message.guild?.emojis.cache.size}`, true)
-			.setFooter("@CodeFictionist")
-			.setTimestamp();
-		message.channel.send({ embed: embed });
-		this.client.embed(message.author);
+			.addField("Emojis:- ", `${message.guild?.emojis.cache.size}`, true);
+
+		this.client.sendEmbed(message, serverInfoEmbed);
 	}
 }
