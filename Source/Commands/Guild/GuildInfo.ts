ae2f3f275c1c87eb7b54ae3d5cb42b2f608278b7
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-15 08:30:44 +0530

    Made region dynamic, & guild is a variable now, instead of message.guild all the time

--- Source/Commands/Guild/GuildInfo.ts
+++ Source/Commands/Guild/GuildInfo.ts
@@ -1,5 +1,5 @@
 import { stripIndents } from "common-tags";
-import { Message } from "discord.js";
+import { Guild, Message } from "discord.js";
 import BaseCommand from "../../Base/BaseCommand";
 import CodeFictionistClient from "../../Base/Client";
 
@@ -13,27 +13,30 @@ export default class ServerInfo extends BaseCommand {
 		});
 	}
 	async run(message: Message) {
+		// @ts-ignore
+		const guild: Guild = message.guild;
+
 		const serverInfoEmbed = this.client.embed(message.author)
-			.setThumbnail(`${message.guild?.iconURL()}`)
-			.setTitle(`${message.guild?.name}`)
-			.addField("Owner", `<@!${message.guild?.ownerID}>`, true)
-			.addField("ID", `${message.guild?.id}`, true)
-			.addField("Created At", `${message.guild?.createdAt.toLocaleDateString("en-us")} ${message.guild?.createdAt.toLocaleTimeString("en-us")}`)
-			.addField("Region", "India", true)
-			.addField("Verification Level", `${message.guild?.verificationLevel.split("").map((str, position) => {
+			.setThumbnail(`${guild.iconURL()}`)
+			.setTitle(`${guild.name}`)
+			.addField("Owner", `<@!${guild.ownerID}>`, true)
+			.addField("ID", `${guild.id}`, true)
+			.addField("Created At", `${guild.createdAt.toLocaleDateString("en-us")} ${guild.createdAt.toLocaleTimeString("en-us")}`)
+			.addField("Region", guild.region, true)
+			.addField("Verification Level", `${guild.verificationLevel.split("").map((str, position) => {
 				if(position === 0) return str.toUpperCase();
 				else return str.toLowerCase();
 			}).join("")}`, true)
-			.addField("Total Members", `${message.guild?.memberCount}`, true)
-			.addField("Bots", `${message.guild?.members.cache.filter(m => m.user.bot).size || 0}`, true)
-			.addField("Boosters", `${message.guild?.premiumSubscriptionCount || 0}`, true)
-			.addField("Roles", `${message.guild?.roles.cache.size}`, true)
-			.addField("Channels", `${message.guild?.channels.cache.filter(ch => ch.type !== "category").size}`, true)
-			.addField("Text Channels", `${message.guild?.channels.cache.filter(ch => (ch.type === "text") || (ch.type === "news") || (ch.type === "store")).size}`, true)
-			.addField("Voice Channels", `${message.guild?.channels.cache.filter(ch => (ch.type === "voice") || (ch.type === "stage")).size}`, true)
+			.addField("Total Members", `${guild.memberCount}`, true)
+			.addField("Bots", `${guild.members.cache.filter(m => m.user.bot).size || 0}`, true)
+			.addField("Boosters", `${guild.premiumSubscriptionCount || 0}`, true)
+			.addField("Roles", `${guild.roles.cache.size}`, true)
+			.addField("Channels", `${guild.channels.cache.filter(ch => ch.type !== "category").size}`, true)
+			.addField("Text Channels", `${guild.channels.cache.filter(ch => (ch.type === "text") || (ch.type === "news") || (ch.type === "store")).size}`, true)
+			.addField("Voice Channels", `${guild.channels.cache.filter(ch => (ch.type === "voice") || (ch.type === "stage")).size}`, true)
 			.addField("Emojis", stripIndents`
-				**Animated:** ${message.guild?.emojis.cache.filter(em => em.animated).size || 0}
-				**Normal:** ${message.guild?.emojis.cache.filter(em => !em.animated).size || 0}
+				**Animated:** ${guild.emojis.cache.filter(em => em.animated).size || 0}
+				**Normal:** ${guild.emojis.cache.filter(em => !em.animated).size || 0}
 			`, true);
 
 		this.client.sendEmbed(message, serverInfoEmbed);
